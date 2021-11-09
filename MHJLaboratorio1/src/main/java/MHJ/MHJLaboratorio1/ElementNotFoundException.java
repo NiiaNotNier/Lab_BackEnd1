116305d232892951c04731fd0b22ed463aac41f6
a24956 <a24956@svalero.com>
2021-11-09 20:27:29 +0100

    change of folder

--- MHJLaboratorio1/src/main/java/MHJ/MHJLaboratorio1/ElementNotFoundException.java
+++ MHJLaboratorio1/src/main/java/MHJ/MHJLaboratorio1/ElementNotFoundException.java
@@ -6,4 +6,4 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No element found")
 public class ElementNotFoundException extends RuntimeException {
 
-}
+} 
