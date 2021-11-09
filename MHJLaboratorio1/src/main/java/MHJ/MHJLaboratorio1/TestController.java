44ddc3baa8fb786fae6ecf28057322a9e27ae16e
Javier Sañudo <sanudo.gregorio@gmail.com>
2021-11-09 19:54:51 +0100

    Added different languages

--- MHJLaboratorio1/src/main/java/MHJ/MHJLaboratorio1/TestController.java
+++ MHJLaboratorio1/src/main/java/MHJ/MHJLaboratorio1/TestController.java
@@ -2,12 +2,11 @@ package MHJ.MHJLaboratorio1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
     @GetMapping("/testHtml")
-    public ModelAndView Test() {
-        return new ModelAndView("test");
+    public String Test() {
+        return "test";
     }
 }
