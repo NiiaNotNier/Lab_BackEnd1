f5bb1448cafba2493567d6d3675bca4fe19bc349
Javier Sañudo <sanudo.gregorio@gmail.com>
2021-11-09 20:39:11 +0100

    Modified message's  old properties and added new properties for the HTML

--- MHJLaboratorio1/src/main/java/MHJ/MHJLaboratorio1/TestController.java
+++ MHJLaboratorio1/src/main/java/MHJ/MHJLaboratorio1/TestController.java
@@ -2,11 +2,12 @@ package MHJ.MHJLaboratorio1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
     @GetMapping("/testHtml")
-    public String Test() {
-        return "test";
+    public ModelAndView Test() {
+        return new ModelAndView("test");
     }
 }
